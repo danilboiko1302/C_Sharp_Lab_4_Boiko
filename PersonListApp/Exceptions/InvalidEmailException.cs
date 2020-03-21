c6e612606c571a7b8211e5ec0af5727a3460ba4a
danilboiko1302 <51995092+danilboiko1302@users.noreply.github.com>
2020-03-21 15:47:46 +0200

    laba_4

---
+++ PersonListApp/Exceptions/InvalidEmailException.cs
@@ -0,0 +1,12 @@
+﻿﻿using System;
+
+namespace PersonListApp.Exceptions
+{
+    internal class InvalidEmailException : Exception
+    {
+        public InvalidEmailException(string message) : base(message)
+        { }
+        public InvalidEmailException(string message, Exception inner) : base(message, inner)
+        { }
+    }
+}
---
+++ PersonListApp/Tools/ILoaderOwner.cs
@@ -0,0 +1,11 @@
+﻿﻿using System.ComponentModel;
+using System.Windows;
+
+namespace PersonListApp.Tools
+{
+    internal interface ILoaderOwner : INotifyPropertyChanged
+    {
+        Visibility LoaderVisibility { get; set; }
+        bool IsControlEnabled { get; set; }
+    }
+}
